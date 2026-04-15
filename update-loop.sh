3c65b36e5e680610304594a9cca99bd418ddd5d0
hfukada <hiroshi@fukada.us>
2026-04-15 11:31:09 +0900

    update-loop, restart all

--- update-loop.sh
+++ update-loop.sh
@@ -1,6 +1,6 @@
 export GIT_COMMIT=$(git rev-parse HEAD)
 export GIT_DATETIME=$(git log -1 --format=%cI)
-docker compose up -d --build hoto
+docker compose up -d --build
 
 while true; do
   if git pull gitea | grep -qv "Already up to date"; then
@@ -8,7 +8,7 @@ while true; do
     export GIT_COMMIT=$(git rev-parse HEAD)
     export GIT_DATETIME=$(git log -1 --format=%cI)
     docker image rm -f hoto-sandbox:latest
-    docker compose up -d --build hoto
+    docker compose up -d --build
   fi
   sleep 30
 done
