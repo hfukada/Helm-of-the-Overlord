9aef2abd690d44158db404ef2de51045dcace9dc
hfukada <hiroshi@fukada.us>
2026-04-22 12:45:09 +0900

    branch tracking on update loop

--- update-loop.sh
+++ update-loop.sh
@@ -3,8 +3,8 @@ export GIT_DATETIME=$(git log -1 --format=%cI)
 docker compose up -d --build
 
 while true; do
-  if git pull gitea | grep -qv "Already up to date"; then
-    git push origin
+  if git pull gitea main | grep -qv "Already up to date"; then
+    git push origin main
     export GIT_COMMIT=$(git rev-parse HEAD)
     export GIT_DATETIME=$(git log -1 --format=%cI)
     docker image rm -f hoto-sandbox:latest
